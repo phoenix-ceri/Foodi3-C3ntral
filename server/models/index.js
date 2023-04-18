09fab2ea2ab42d7182c4015b71da22d5434c529b
Phoenix-Ceri <phoenixceri@protonmail.com>
2023-04-18 19:02:20 -0400

    Let there be Code(added the boiler plate)

--- server/models/index.js
+++ server/models/index.js
@@ -1,4 +1,6 @@
-const Tech = require('./Tech');
-const Matchup = require('./Matchup');
+const User = require('./User');
+const Product = require('./Product');
+const Category = require('./Category');
+const Order = require('./Order');
 
-module.exports = { Tech, Matchup };
+module.exports = { User, Product, Category, Order };
