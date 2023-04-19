2f7aeed9e238d1a453311e788583ba16b52735cb
Phoenix-Ceri <phoenixceri@protonmail.com>
2023-04-19 16:57:46 -0400

    started work on navigation

--- newsrc/components/nav.js
+++ newsrc/components/nav.js
@@ -1,1 +1,11 @@
-phoenix
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Navbar = () => {
+    return (
+[]
+// add navigation bar  code
+    );
+  };
+  
+  export default Navbar;
