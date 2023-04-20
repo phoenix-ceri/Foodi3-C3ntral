1c958a266d9e19c2341df111a1b386505e2abb43
Phoenix-Ceri <phoenixceri@protonmail.com>
2023-04-20 15:05:25 -0400

    rough of the three components

--- newsrc/components/aboutus.js
+++ newsrc/components/aboutus.js
@@ -1,1 +1,53 @@
-phoenix
+import React from 'react';
+import { Container, Row, Col, Image } from 'react-bootstrap';
+
+function AboutTeamPage() {
+  const teamMembers = [
+    {
+      name: 'John Doe',
+      title: 'CEO',
+      image: 'https://via.placeholder.com/150',
+      bio:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut ac metus eget velit suscipit commodo a sed nulla.',
+    },
+    {
+      name: 'Jane Doe',
+      title: 'CTO',
+      image: 'https://via.placeholder.com/150',
+      bio:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut ac metus eget velit suscipit commodo a sed nulla.',
+    },
+    {
+      name: 'Bob Smith',
+      title: 'Lead Developer',
+      image: 'https://via.placeholder.com/150',
+      bio:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut ac metus eget velit suscipit commodo a sed nulla.',
+    },
+    {
+      name: 'Sara Lee',
+      title: 'Marketing Manager',
+      image: 'https://via.placeholder.com/150',
+      bio:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut ac metus eget velit suscipit commodo a sed nulla.',
+    },
+  ];
+
+  return (
+    <Container>
+      <h1>About the Team</h1>
+      <Row>
+        {teamMembers.map((member, index) => (
+          <Col key={index}>
+            <Image src={member.image} roundedCircle />
+            <h3>{member.name}</h3>
+            <h4>{member.title}</h4>
+            <p>{member.bio}</p>
+          </Col>
+        ))}
+      </Row>
+    </Container>
+  );
+}
+
+export default AboutTeamPage;
--- newsrc/components/navOne.js
+++ newsrc/components/navOne.js
@@ -1,11 +1,24 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Navbar, Nav } from 'react-bootstrap';
 
-const Navbar = () => {
-    return (
-[]
-// add navigation bar  code
-    );
-  };
-  
-  export default Navbar;
+function navbar() {
+  return (
+    <div>
+      <Navbar bg="light" expand="lg" sticky="top">
+        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="ml-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#link">Link</Nav.Link>
+            <Nav.Link href="#about">About</Nav.Link>
+            <Nav.Link href="#contact">Contact</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+      <div style={{ height: '1500px' }}>Scroll down to see the effect of sticky-top</div>
+    </div>
+  );
+}
+
+export default navbar;
