85abf28b061b73f8ab91be0f4b70bb86390a84cd
kflinnn <kflinncoding@gmail.com>
2023-04-25 16:38:24 -0400

    added axios npm, deleted duplicate folder

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -1,3 +1,5 @@
+const API_KEY = process.env.SPOONACULAR_API_KEY;
+
 export default function spoonacular (search) {
     return fetch (`https://api.spoonacular.com/recipes/complexSearch?query=${search}`).then(response => {
         return response.json()
