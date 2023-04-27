fc532ef6d2f576817dc44929fcf997a220959a04
GeckoGiggles <eva.judith.perez@gmail.com>
2023-04-27 05:39:26 -0400

    styled profile, changed bot nav and footer and side nav components

--- client/src/components/BotBar/index.js
+++ client/src/components/BotBar/index.js
@@ -4,7 +4,7 @@ import { Navbar, Nav } from 'react-bootstrap';
 const BottomNavbar = () => {
   return (
     <Navbar bg="dark" variant="dark" className='botnav'>
-      <Nav className="m-auto">
+      <Nav className="mx-auto pb-5 mb-5">
         <Nav.Link href="#Home">Home</Nav.Link>
         <Nav.Link href="#about">About Us</Nav.Link>
         <Nav.Link href="#contact">Recipes</Nav.Link>
