18a6751f26682506407f258f71cbc4f7bd2ea6f8
kflinnn <kflinncoding@gmail.com>
2023-04-27 23:41:16 -0400

    troubleshooting signup

--- client/src/pages/RecipePage/searchRecipe.js
+++ client/src/pages/RecipePage/searchRecipe.js
@@ -1,7 +1,7 @@
 import React from "react";
 import Search from "../../components/Recipes/recipeSearch";
 import HeroImage from "../../components/Hero";
-import SideBar from "../../components/Sidebar";
+import SideBar from "../../components/SideBar";
 
 const SearchPage = () => {
     return (
