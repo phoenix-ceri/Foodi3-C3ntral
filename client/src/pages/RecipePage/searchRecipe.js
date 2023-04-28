2da23746785e7ac4e14847355ec0af8e6a006267
Phoenix-Ceri <phoenixceri@protonmail.com>
2023-04-28 19:22:15 -0400

    changed to snav for side bar

--- client/src/pages/RecipePage/searchRecipe.js
+++ client/src/pages/RecipePage/searchRecipe.js
@@ -1,7 +1,7 @@
 import React from "react";
 import Search from "../../components/Recipes/recipeSearch";
 import HeroImage from "../../components/Hero";
-import SideBar from "../../components/SideBar";
+import SideBar from "../../components/SNav";
 import TopBar from "../../components/TopBar";
 
 const SearchPage = () => {
