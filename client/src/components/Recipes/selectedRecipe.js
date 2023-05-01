eb3c147171286ed5312c55aa2de3ef7cafc0f7ff
kflinnn <kflinncoding@gmail.com>
2023-05-01 00:29:31 -0400

    deleted problematic npm package, commented out the comment section

--- client/src/components/Recipes/selectedRecipe.js
+++ client/src/components/Recipes/selectedRecipe.js
@@ -2,8 +2,8 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import RecipeModal from '../../components/Modal/modal';
 import Auth from "../../utils/auth";
-import ReviewList from '../../components/Comments/render';
-import AddReview from '../Comments/add';
+// import ReviewList from '../../components/Comments/render';
+// import AddReview from '../Comments/add';
 import RecipeCard from './recipeCard';
 import './index.scss'
 
@@ -15,13 +15,13 @@ function SelectedRecipe() {
       <RecipeCard />
       {Auth.loggedIn() ? (
         <div>
-          <ReviewList />
+          {/* <ReviewList /> */}
           <RecipeModal />
-          <AddReview />
+          {/* <AddReview /> */}
         </div>
       ) : (
         <>
-              <ReviewList />
+              {/* <ReviewList /> */}
                 <div className='suggestion'>Would you like to <Link className='loginLinkSuggestion' to='/login'>Log in</Link> or <Link className='loginLinkSuggestion' to="/signup">Sign up </Link>to leave a review and/or save this recipe to your meal plan?</div>
                 </>
       )}
