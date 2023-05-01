38d7bb6ece011789a9fec40dc7b4833077fce6cd
GeckoGiggles <eva.judith.perez@gmail.com>
2023-05-01 01:51:42 -0400

    got rid of some things

--- client/src/components/Comments/render.js
+++ client/src/components/Comments/render.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Card, ListGroup, ListGroupItem } from 'react-bootstrap';
-import StarRatingComponent from 'react-star-rating-component';
+
 
 
 const ReviewList = ({ reviews = [] }) => {
