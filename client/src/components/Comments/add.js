84c9242d8c155bda2638572ef5c026d887f286fe
kflinnn <kflinncoding@gmail.com>
2023-05-01 17:26:59 -0400

    debugged backend

--- client/src/components/Comments/add.js
+++ client/src/components/Comments/add.js
@@ -1,119 +1,119 @@
-import { useMutation } from "@apollo/client";
-import React, { useState } from "react";
-import { Card, Button, Form } from "react-bootstrap";
-import { FaStar } from "react-icons/fa";
-import { ADD_RATING } from "../../utils/mutations";
-import Auth from '../../utils/auth';
+// import { useMutation } from "@apollo/client";
+// import React, { useState } from "react";
+// import { Card, Button, Form } from "react-bootstrap";
+// import { FaStar } from "react-icons/fa";
+// import { ADD_RATING } from "../../utils/mutations";
+// import Auth from '../../utils/auth';
 
-function AddReview({ recipeDetailsId }) {
-  const [rating, setRating] = useState(0);
-  const [characterCount, setCharacterCount] = useState(0);
-  const [commentBody, setCommentBody] = useState("");
+// function AddReview({ recipeDetailsId }) {
+//   const [rating, setRating] = useState(0);
+//   const [characterCount, setCharacterCount] = useState(0);
+//   const [commentBody, setCommentBody] = useState("");
   
-  const [addRating, { error }] = useMutation(ADD_RATING);
+//   const [addRating, { error }] = useMutation(ADD_RATING);
 
-  const handleFormSubmit = async (event) => {
-    event.preventDefault();
+//   const handleFormSubmit = async (event) => {
+//     event.preventDefault();
 
-    try {
-        const { data } = await addRating({
-            variables: {
-                recipeDetailsId,
-                commentBody,
-                commentAuthor: Auth.getProfile().data.username,
-            },
-        });
+//     try {
+//         const { data } = await addRating({
+//             variables: {
+//                 recipeDetailsId,
+//                 commentBody,
+//                 commentAuthor: Auth.getProfile().data.username,
+//             },
+//         });
 
-        setRating(0);
-        setCommentBody('');
-    } catch (err) {
-        console.error(err);
-    }
-  };
+//         setRating(0);
+//         setCommentBody('');
+//     } catch (err) {
+//         console.error(err);
+//     }
+//   };
   
-  const handleChange = (event) => {
-    const { name, value } = event.target;
+//   const handleChange = (event) => {
+//     const { name, value } = event.target;
 
-    if (name === 'commentBody' && value.length <= 280) {
-        setCommentBody(value);
-        setCharacterCount(value.length);
-    } else if (name === 'rating' && value >= 0 && value <= 5) {
-        setRating(parseInt(value));
-    }
-  };
+//     if (name === 'commentBody' && value.length <= 280) {
+//         setCommentBody(value);
+//         setCharacterCount(value.length);
+//     } else if (name === 'rating' && value >= 0 && value <= 5) {
+//         setRating(parseInt(value));
+//     }
+//   };
 
  
-  return (
-    <section style={{ backgroundColor: "#d94125" }}>
-      <div className="container my-5 py-5 text-dark">
-        <div className="row d-flex justify-content-center">
-          <div className="col-md-10 col-lg-8 col-xl-6">
-            <Card>
-              <Card.Body className="p-4">
-                <div className="d-flex flex-start w-100">
-                  <img
-                    className="rounded-circle shadow-1-strong me-3"
-                    src="https://mdbcdn.b-cdn.net/img/Photos/Avatars/img%20(21).webp"
-                    alt="avatar"
-                    width="65"
-                    height="65"
-                  />
-                  <div className="w-100">
-                    <div className="d-flex align-items-center mb-3">
-                      {[...Array(5)].map((_, index) => (
-                        <FaStar
-                          key={index}
-                          className="star"
-                          color={index < rating ? "red" : "gray"}
-                          size={20}
-                          title={
-                            index === 0
-                              ? "Bad"
-                              : index === 1
-                              ? "Poor"
-                              : index === 2
-                              ? "OK"
-                              : index === 3
-                              ? "Good"
-                              : "Excellent"
-                          }
-                          onClick={() => setRating(index + 1)}
-                          onMouseEnter={() => setRating(index + 1)}
-                          onMouseLeave={() => setRating(0)}
-                        />
-                      ))}
-                    </div>
-                    <Form onSubmit={handleFormSubmit}>
-                      <Form.Group controlId="comment">
-                        <Form.Label>Leave a Review!</Form.Label>
-                        <Form.Control
-                          input="text"
-                          as="textarea"
-                          rows={4}
-                          value={commentBody}
-                          onChange={handleChange}
-                        />
-                        <Form.Text className="text-muted">
-                            {characterCount}/280
-                        </Form.Text>
-                      </Form.Group>
-                      <div className="d-flex justify-content-between mt-3">
-                        <Button variant="success">Delete</Button>
-                        <Button type="submit" variant="danger">
-                          Send <i className="fas fa-long-arrow-alt-right ms-1"></i>
-                        </Button>
-                      </div>
-                    </Form>
-                  </div>
-                </div>
-              </Card.Body>
-            </Card>
-          </div>
-        </div>
-      </div>
-    </section>
-  );
-}
+//   return (
+//     <section style={{ backgroundColor: "#d94125" }}>
+//       <div className="container my-5 py-5 text-dark">
+//         <div className="row d-flex justify-content-center">
+//           <div className="col-md-10 col-lg-8 col-xl-6">
+//             <Card>
+//               <Card.Body className="p-4">
+//                 <div className="d-flex flex-start w-100">
+//                   <img
+//                     className="rounded-circle shadow-1-strong me-3"
+//                     src="https://mdbcdn.b-cdn.net/img/Photos/Avatars/img%20(21).webp"
+//                     alt="avatar"
+//                     width="65"
+//                     height="65"
+//                   />
+//                   <div className="w-100">
+//                     <div className="d-flex align-items-center mb-3">
+//                       {[...Array(5)].map((_, index) => (
+//                         <FaStar
+//                           key={index}
+//                           className="star"
+//                           color={index < rating ? "red" : "gray"}
+//                           size={20}
+//                           title={
+//                             index === 0
+//                               ? "Bad"
+//                               : index === 1
+//                               ? "Poor"
+//                               : index === 2
+//                               ? "OK"
+//                               : index === 3
+//                               ? "Good"
+//                               : "Excellent"
+//                           }
+//                           onClick={() => setRating(index + 1)}
+//                           onMouseEnter={() => setRating(index + 1)}
+//                           onMouseLeave={() => setRating(0)}
+//                         />
+//                       ))}
+//                     </div>
+//                     <Form onSubmit={handleFormSubmit}>
+//                       <Form.Group controlId="comment">
+//                         <Form.Label>Leave a Review!</Form.Label>
+//                         <Form.Control
+//                           input="text"
+//                           as="textarea"
+//                           rows={4}
+//                           value={commentBody}
+//                           onChange={handleChange}
+//                         />
+//                         <Form.Text className="text-muted">
+//                             {characterCount}/280
+//                         </Form.Text>
+//                       </Form.Group>
+//                       <div className="d-flex justify-content-between mt-3">
+//                         <Button variant="success">Delete</Button>
+//                         <Button type="submit" variant="danger">
+//                           Send <i className="fas fa-long-arrow-alt-right ms-1"></i>
+//                         </Button>
+//                       </div>
+//                     </Form>
+//                   </div>
+//                 </div>
+//               </Card.Body>
+//             </Card>
+//           </div>
+//         </div>
+//       </div>
+//     </section>
+//   );
+// }
 
-export default AddReview;
+// export default AddReview;
 
--- client/src/components/Login/index.js
+++ client/src/components/Login/index.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { useMutation } from '@apollo/client';
 import Auth from '../../utils/auth';
-import { Link, Router, useNavigate } from 'react-router-dom'
+import { Link } from 'react-router-dom'
 import { LOGIN } from '../../utils/mutations'
 import { Container, Row, Col, Form, Button } from "react-bootstrap";
 import "./index.scss";
@@ -72,44 +72,6 @@ const Login = (props) => {
         </Row>
       </Container>
     </div>
-
-    //   <div className="container my-1">
-
-    //   <h2>Login</h2>
-    //   <form onSubmit={handleFormSubmit}>
-    //     <div className="flex-row space-between my-2">
-    //       <label htmlFor="email">Email address:</label>
-    //       <input
-    //         placeholder="youremail@test.com"
-    //         name="email"
-    //         type="email"
-    //         id="email"
-    //         onChange={handleChange}
-    //       />
-    //     </div>
-    //     <div className="flex-row space-between my-2">
-    //       <label htmlFor="pwd">Password:</label>
-    //       <input
-    //         placeholder="******"
-    //         name="password"
-    //         type="password"
-    //         id="pwd"
-    //         onChange={handleChange}
-    //       />
-    //     </div>
-    //     {error ? (
-    //       <div>
-    //         <p className="error-text">The provided credentials are incorrect</p>
-    //       </div>
-    //     ) : null}
-    //     <div className="flex-row flex-end">
-    //       <button type="submit">Submit</button>
-    //     </div>
-    //   </form>
-    //   <p>
-    //   <Link to="/signup">← Go to Signup</Link>
-    //   </p>
-    // </div>
   );
 };
 
